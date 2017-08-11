58b045515d8cd038bd53258e5311efa1c0cbc52d
Pedro Freitas <dryginlogic@gmail.com>
2017-08-11 21:03:31 +0100

    Tweak timehop service

--- timehop_bot_service.rb
+++ timehop_bot_service.rb
@@ -2,6 +2,13 @@ require 'dotenv/load'
 require 'telegram_bot'
 require_relative './flickr_indio'
 
+def send_msg(msg_text)
+  message = TelegramBot::OutMessage.new(chat: @channel)
+  puts msg_text
+  message.text = msg_text
+  message.send_with(@bot)
+end
+
 if ARGV[0] == 'live'
   @mode = :live
   @telegram_channel_id = ENV.fetch('CHANNEL_ID_LIVE')
@@ -13,28 +20,28 @@ else
 end
 
 fi = FlickrIndio.new
-bot = TelegramBot.new(token: @bot_token)
+@bot = TelegramBot.new(token: @bot_token)
+@channel = TelegramBot::Channel.new(id: @telegram_channel_id)
 
 puts "TimeHop Service starting"
 
-channel = TelegramBot::Channel.new(id: @telegram_channel_id)
-message = TelegramBot::OutMessage.new(chat: channel)
-
 # fetch sets on the same day as today
 date = Date.today
-photos = fi.timehop(date)
+photos = fi.timehop(date).reverse
 
 puts "#{photos.size} events to report!"
 
 if photos.any?
-  # send set info and pics
+  # send title and set info
   msg_text = "~~~ INDIOS TIMEHOP ~~~\nThere were #{photos.size} events on #{date.strftime('%B %-d')}:\n"
   photos.each do |photo|
-    msg_text += "#{photo['title']}: #{photo['set_url']}\n"
+    msg_text += " - #{photo['title']}\n"
   end
+  # send title message
+  send_msg msg_text
 
-  # send message
-  puts msg_text
-  message.text = msg_text
-  message.send_with(bot)
+  photos.each do |photo|
+    sleep 5
+    send_msg "#{photo['url']}\nTag this img with: /tagid #{photo['id']} ..."
+  end
 end
